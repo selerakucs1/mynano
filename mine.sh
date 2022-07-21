fb506b11eb08b7429116549863d05b04fc176fea
selerakucs1 <40531122+selerakucs1@users.noreply.github.com>
2022-07-21 07:48:08 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,3 +1,4 @@
 wget https://github.com/nanopool/nanominer/releases/download/v3.6.8/nanominer-linux-3.6.8.tar.gz
 tar -xvf nanominer-linux-3.6.8.tar.gz
 cd nanominer-linux-3.6.8
+cp start.ini /nanominer-linux-3.6.8
