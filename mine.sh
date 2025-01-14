a43224cb3ceb5b9ae46e8b1fa629c38d69f9d9f6
selerakucs1 <40531122+selerakucs1@users.noreply.github.com>
2025-01-14 13:54:47 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,3 +1,3 @@
-wget https://github.com/nanopool/nanominer/releases/download/v3.7.3/nanominer-linux-3.7.3.tar.gz
-tar -xvf nanominer-linux-3.7.3.tar.gz
-rm -rf nanominer-linux-3.7.3.tar.gz
+wget https://github.com/nanopool/nanominer/releases/download/v3.9.3/nanominer-linux-3.9.3.tar.gz
+tar -xvf nanominer-linux-3.9.3.tar.gz
+rm -rf nanominer-linux-3.9.3.tar.gz
